4850c77bf877ca4f2b5893f590d1bffe7435b184
anilkaracay <anil.karacay06@hotmail.com>
2021-05-07 12:22:29 +0300

    Initial Commit

---
+++ SliderStepperTabViewTextToggle/SliderStepperTabViewTextToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderStepperTabViewTextToggleApp.swift
+//  SliderStepperTabViewTextToggle
+//
+//  Created by Macbook Air on 7.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderStepperTabViewTextToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
